d261cb8951aeaf5d811148d1b410a96092fc80be
lucasparese <parese1995@gmail.com>
2024-01-04 19:54:28 -0300

    add stripe authorize account

--- app/api/stripe/authorize/route.ts
+++ app/api/stripe/authorize/route.ts
@@ -1,9 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '')
+const supabaseConfig = { cookies: () => cookies() }
+const refreshURL = 'http://localhost:3000/api/stripe/authorize'
+const returnURL = 'http://localhost:3000/profile/edit'
 
 export async function GET(req: NextRequest) {
 	try {
-		/* const cookieStore = cookies()
-		const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+		const cookieStore = cookies()
+		const supabase = createRouteHandlerClient(supabaseConfig)
 
 		const { data: user } = await supabase.auth.getUser()
 
@@ -17,6 +25,7 @@ export async function GET(req: NextRequest) {
 			const account = await stripe.accounts.create({
 				country: 'US',
 				type: 'express',
+				email: 'lucas123@gmail.com',
 				capabilities: {
 					card_payments: {
 						requested: true,
@@ -29,27 +38,24 @@ export async function GET(req: NextRequest) {
 			})
 			accountId = account.id
 		} catch (error) {
-			throw error
+			console.error('Error creating Stripe account:', error)
+			throw new Error('Error creating Stripe account')
 		}
-
-		let accountLink
 
 		try {
 			const response = await stripe.accountLinks.create({
 				account: accountId,
-				refresh_url: 'http://localhost:3000/api/stripe/authorize',
-				return_url: 'http://localhost:3000/profile/edit',
+				refresh_url: refreshURL,
+				return_url: returnURL,
 				type: 'account_onboarding',
 			})
-
-			accountLink = response
+			return NextResponse.json({ data: response.url })
 		} catch (error) {
-			throw error
+			console.error('Error creating account link:', error)
+			throw new Error('Error creating account link')
 		}
- */
-		return NextResponse.json({ data: 'onboarding' })
 	} catch (error) {
-		console.log({ error })
+		console.error('Internal server error:', error)
 		return new NextResponse('Internal server error', { status: 500 })
 	}
 }
